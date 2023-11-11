68bad1385ea7febb92d812184f0e147b38e8586d
sameerpw43 <89029265+sameerpw43@users.noreply.github.com>
2023-11-11 13:52:08 +0530

    changed

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -26,7 +26,8 @@ const About = () => {
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
+      <span className="italic">When I&apos;m not coding</span>
+, I enjoy playing
         video games, watching movies,cooking and Exercise. I also enjoy
         <span className="font-medium">learning new things</span>. I am currently
         learning about
