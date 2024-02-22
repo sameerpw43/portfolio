0ad95b5e48f3fce27ac16208f712c0b9d04c2021
sameerpw43 <89029265+sameerpw43@users.noreply.github.com>
2024-02-22 11:59:33 +0530

    Update Intro.tsx

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -35,7 +35,7 @@ const Intro = () => {
       animate={{opacity:1,y:0}}>
       <span className="font-bold">Hello, I&apos;m Sameer Khan.</span>
  I&apos;m 
-        <span className="font-bold">Software Engineer</span> with
+        <span className="font-bold"> Software Engineer</span> with
         <span className="font-bold"> 10 months</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is
         <span className="underline">React (&#40;Mern Stack&#41;)</span>.
