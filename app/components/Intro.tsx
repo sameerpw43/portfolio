3720ecb6d24c0f76e05f9d7db0cb47fbce52e9b0
sameerpw43 <89029265+sameerpw43@users.noreply.github.com>
2024-05-18 10:55:25 +0530

    Update Intro.tsx

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -36,7 +36,7 @@ const Intro = () => {
       <span className="font-bold">Hello, I&apos;m Sameer Khan.</span>
  I&apos;m 
         <span className="font-bold"> Software Engineer</span> with
-        <span className="font-bold"> 10 months</span> of experience. I enjoy
+        <span className="font-bold"> 12 months</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is
         <span className="underline">React (&#40;Mern Stack&#41;)</span>.
 
