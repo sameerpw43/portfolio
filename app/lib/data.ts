9ee179fe8b81f74bb5c796783da6c0518537ac55
sameerpw43 <89029265+sameerpw43@users.noreply.github.com>
2024-05-18 11:34:08 +0530

    Update data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -57,9 +57,9 @@ export const experiencesData = [
     title: "Software Engineer",
     location: "Hyderabad,Telangana,India",
     description:
-    "I'm  currently working as full-stack software engineer with  of experience building engaging and scalable web applications. Currently at AptCare.ai, a company focused on revolutionizing the healthcare industry, I leverage my mastery of the MERN stack, Next.js for server-side rendering, and Tailwind CSS,MUI, for rapid UI development"
+    "I'm  currently working as full-stack software engineer with  of experience building engaging and scalable web applications. Currently at AptCare.ai, a company focused on revolutionizing the healthcare industry, I leverage my mastery of the MERN stack, Next.js for server-side rendering, and Tailwind CSS,MUI, for rapid UI development",
     icon: React.createElement(CgWorkAlt),
-    date: "2023 - 2024",
+    date: "2023 - Current",
   },
   
 ] as const;
