7b7cb5f685e8c5411893301e1f4777d8d2c13d76
sameerpw43 <89029265+sameerpw43@users.noreply.github.com>
2025-12-21 21:34:11 +0530

    Update experience duration in Intro component
    Updated experience from 2.1 to 2.7 years in the Intro component.

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -36,7 +36,7 @@ const Intro = () => {
       <span className="font-bold">Hello, I&apos;m Sameer Khan.</span>
  I&apos;m 
         <span className="font-bold"> Software Engineer</span> with
-        <span className="font-bold"> 2.1 year</span> of experience. I enjoy
+        <span className="font-bold"> 2.7 year</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is
         <span className="underline">React  (&#40;Mern Stack&#41;)</span>.
 
@@ -59,3 +59,4 @@ const Intro = () => {
 }
 
 export default Intro
+
